4ccd2e8d62187c43d5b5c3595739b6520e939094
Juan Esteban <trianajuan28@gmail.com>
2025-03-26 20:33:00 -0500

    26 de marzo

---
+++ Back/inmobiliaria/src/main/java/co/edu/usbcali/inmobiliaria/controller/TipoPropiedadController.java
@@ -0,0 +1,9 @@
+package co.edu.usbcali.inmobiliaria.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tipo-propiedad")
+public class TipoPropiedadController {
+}
--- Back/inmobiliaria/src/main/java/co/edu/usbcali/inmobiliaria/repository/TipoPropiedadRepository.java
+++ Back/inmobiliaria/src/main/java/co/edu/usbcali/inmobiliaria/repository/TipoPropiedadRepository.java
@@ -2,6 +2,8 @@ package co.edu.usbcali.inmobiliaria.repository;
 
 import co.edu.usbcali.inmobiliaria.model.TipoPropiedad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TipoPropiedadRepository extends JpaRepository<TipoPropiedad, Integer> {
 }
